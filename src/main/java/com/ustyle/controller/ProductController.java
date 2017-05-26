fdf76889b5aec32d2c41bdf96d0cec3d2f309879
xorwnsnmnnhlba <xorwnsnmnn@naver.com>
2017-05-26 21:48:17 +0900

    상품정보 업로드 검증 작업 진행. 이때 Ajax를 이용하여 이미지 파일 업로드 시, 명확하게 이미지 파일만 업로드되게 함

--- src/main/java/com/ustyle/controller/ProductController.java
+++ src/main/java/com/ustyle/controller/ProductController.java
@@ -1,15 +1,24 @@
 package com.ustyle.controller;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ustyle.domain.Product;
+import com.ustyle.domain.User;
 import com.ustyle.service.ProductService;
+import com.ustyle.utils.ProductUploadValidator;
 
 @Controller
 @RequestMapping("/admin/product/*")
@@ -19,6 +28,9 @@ public class ProductController {
 	
 	@Inject
 	private ProductService service;
+	
+	@Inject
+	private ProductUploadValidator validator;
 
 	@RequestMapping(value = "addProduct.do", method = RequestMethod.GET)
 	public String addProductForm() {
@@ -26,12 +38,33 @@ public class ProductController {
 	}
 	
 	@RequestMapping(value = "addProduct.do", method = RequestMethod.POST)
-	public String addProduct(Product product) throws Exception {
-		logger.info(product.toString());
+	public ModelAndView addProduct(@ModelAttribute @Valid Product product, BindingResult bindingResult) throws Exception {
+
+		validator.validate(product, bindingResult);
+		String[] files = product.getFiles();
+		
+		if ( files != null )		// 업로드할 상품의 이미지가 존재하는 경우
+		{
+			String filesStr = Arrays.toString(files);	
+			product.setPictureUrl(filesStr);
+		}
+		
+		logger.info(product.toString());	
+		
+		ModelAndView mav = new ModelAndView("main/base");
+		
+		if (bindingResult.hasErrors()) {
+			mav.getModel().putAll(bindingResult.getModel());
+			mav.setViewName("product/addProduct");
+			return mav;
+		}
 		
 		service.insert(product);
+		mav.addObject(product);
+		return mav;
+
 		
-		return "redirect:/admin/main.do";
+//		return "redirect:/admin/main.do";
 	}
 	
 	@RequestMapping(value = "addItem.do", method = RequestMethod.GET)
