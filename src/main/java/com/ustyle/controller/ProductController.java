ccab49e710fe1bed646139211bc5ea4b01baf2ea
xorwnsnmnnhlba <xorwnsnmnn@naver.com>
2017-05-27 12:54:44 +0900

    no message

--- src/main/java/com/ustyle/controller/ProductController.java
+++ src/main/java/com/ustyle/controller/ProductController.java
@@ -9,10 +9,12 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ustyle.domain.Product;
@@ -62,9 +64,13 @@ public class ProductController {
 		service.insert(product);
 		mav.addObject(product);
 		return mav;
-
-		
-//		return "redirect:/admin/main.do";
+	}
+	
+	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
+	public void readProduct(@RequestParam("pid") int pid, 
+				 ModelAndView mav) throws Exception
+	{
+//		mav.addAttribute(service.read(bno));
 	}
 	
 	@RequestMapping(value = "addItem.do", method = RequestMethod.GET)
