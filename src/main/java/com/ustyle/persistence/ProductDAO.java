3b264de501ba03570be1f21b45a0e57ca0eb9e63
xorwnsnmnnhlba <xorwnsnmnn@naver.com>
2017-06-04 14:33:01 +0900

    Product에 해당하는 Item 수정 작업 완료

--- src/main/java/com/ustyle/persistence/ProductDAO.java
+++ src/main/java/com/ustyle/persistence/ProductDAO.java
@@ -9,8 +9,8 @@ public interface ProductDAO {
 	public void insert(Product product);
 	public int selectListCnt(HashMap<String, Object> map);
 	public List<Product> productList(HashMap<String, Object> map);
-	public Product read(Integer pid) throws Exception;
-	public String selectPictureUrl(Integer pid) throws Exception;
+	public Product read(Integer productid) throws Exception;
+	public String selectPictureUrl(Integer productid) throws Exception;
 	public void update(Product product);
 	public void delete(int productid);
 	public void deleteItem(int productid);
