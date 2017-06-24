5abcb8f1db5c6ff0ca8612cbe250f4974a584c79
graybat21 <graybat21@gmail.com>
2017-06-24 15:38:10 +0900

    no message

--- src/main/java/com/ustyle/controller/PinController.java
+++ src/main/java/com/ustyle/controller/PinController.java
@@ -1,22 +1,31 @@
-package com.ustyle.controller;
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-
-import com.ustyle.service.ProductService;
-
-@Controller
-public class PinController {
-
-	private static final Logger logger = LoggerFactory.getLogger(PinController.class);
-	
-	@Inject
-	private PinService pinService;
-	@Inject
-	private ProductService productService;
-	
-	
-}
+//package com.ustyle.controller;
+//
+//import javax.inject.Inject;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import com.ustyle.service.PinService;
+//import com.ustyle.service.ProductService;
+//
+//@Controller
+//public class PinController {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(PinController.class);
+//	
+//	@Inject
+//	private PinService pinService;
+//	@Inject
+//	private ProductService productService;
+//	
+//	@RequestMapping("pin.do")
+//	public ModelAndView myPinList() throws Exception{
+//		ModelAndView mav=new ModelAndView("pin/ ? /My Pin Board");
+//		
+//		return mav;
+//	}
+//	
+//}
---
+++ src/main/java/com/ustyle/service/PinService.java
@@ -0,0 +1,5 @@
+package com.ustyle.service;
+
+public interface PinService {
+	
+}
