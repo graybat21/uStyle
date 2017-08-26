10602d888ddaa219bbc1e0a7ce598ed9c7ce915c
graybat21 <graybat21@gmail.com>
2017-08-26 21:30:38 +0900

    qna 마지막 번호 가져오는 메소드 쿼리문 수정

--- src/main/java/com/ustyle/persistence/QnaDAOImpl.java
+++ src/main/java/com/ustyle/persistence/QnaDAOImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.ustyle.domain.Faq;
 import com.ustyle.domain.Qna;
 @Repository
-public class QnaDAOImple implements QnaDAO {
+public class QnaDAOImpl implements QnaDAO {
 	private static String namespace = "com.ustyle.mappers.qnaMapper";
 	@Inject
 	private SqlSession session;
